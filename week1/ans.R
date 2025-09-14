b8a4a761977ec8e3e95cbb18466e5bb41cc370f1
thiyangt <tstalagala@gmail.com>
2025-09-14 06:46:00 +0530

    updated

---
+++ week1/ans.R
@@ -0,0 +1,25 @@
+- To summarize large amount of data
+in a meaningful way.
+
+- To be efficient with daily work or 
+decision making process
+
+- To simplify complex data
+
+- To transform raw data into
+meaningful insights
+
+- To study data easily
+
+- To communicate easily
+
+- Easy to understand
+
+- To provide foundation for fomulating
+suitable policies
+
+- To make predictions and forecasts
+
+- To save time
+
+- To take data driven decisions
